178185b2b6bfda781847423911633b1dc39a6062
Owen <owen2862@live.com>
2017-09-02 22:13:04 +0800

    日期與時間加入選擇功能

--- Accounting/Models/ViewModels/AccountingDetailViewModel.cs
+++ Accounting/Models/ViewModels/AccountingDetailViewModel.cs
@@ -9,11 +9,13 @@ namespace Accounting.Models.ViewModels {
 
         [Display(Name = "日期")]
         [RemoteDoublePlus("LessThanOrEqualToToday", "Valid", "", ErrorMessage = "日期不得大於今天")]
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
 
         [Display(Name = "金額")]
         //[RemoteDoublePlus("GreaterThanZero", "Valid", "", ErrorMessage = "金額只能輸入正整數")]
         [GreaterThanZero(ErrorMessage = "金額只能輸入正整數")]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
         [Display(Name = "類別")]
